7e6a393cdd83cbac17f916a04d1d57abd4a0e5e5
BlueDress <stra6aro@abv.bg>
2017-05-25 15:12:30 +0300

    Update

---
+++ Programming Fundamentals C#/Regular Expressions/Replace a tag/ReplaceATag.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Replace_a_tag
+{
+    public class ReplaceATag
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (input.Equals("End"))
+                {
+                    break;
+                }
+                else
+                {
+                    var patern = @"<a.*?href.*?=(.*)>(.*?)<\/a>";
+                    var replace = @"[URL href=$1]$2[/URL]";
+                    var regex = new Regex(patern);
+                    var result = regex.Replace(input, replace);
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
