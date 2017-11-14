423dcd8ffc9af6ba2f89d2e002f0e2ffe714a639
BlueDress <stra6aro@abv.bg>
2017-11-14 12:07:59 +0200

    Update

---
+++ Databases Advanced Entity Framework/Code First/P01_HospitalDatabase/Data/HospitalContext.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_HospitalDatabase.Data.Models;
+using System.IO;
+
+namespace P01_HospitalDatabase.Data
+{
+    public class HospitalContext : DbContext
+    {
+        public HospitalContext()
+        {
+
+        }
+
+        public HospitalContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Diagnose> Diagnoses { get; set; }
+        public DbSet<Medicament> Medicaments { get; set; }
+        public DbSet<PatientMedicament> PatientsMedicaments { get; set; }
+        public DbSet<Visitation> Visitations { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = File.ReadAllText("C:\\Program Files (x86)\\Notepad++\\Instructions.txt");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Patient>(e =>
+            {
+                e.HasKey(p => p.PatientId);
+
+                e.Property(p => p.FirstName)
+                .IsRequired()
+                .IsUnicode()
+                .HasMaxLength(50);
+
+                e.Property(p => p.LastName)
+                .IsRequired()
+                .IsUnicode()
+                .HasMaxLength(50);
+
+                e.Property(p => p.Address)
+                .IsRequired()
+                .IsUnicode()
+                .HasMaxLength(250);
+
+                e.Property(p => p.Email)
+                .IsRequired()
+                .IsUnicode(false)
+                .HasMaxLength(80);
+
+                e.Property(p => p.HasInsurance)
+                .HasDefaultValue(true);
+            });
+
+            modelBuilder.Entity<Visitation>(e =>
+            {
+                e.HasKey(v => v.VisitationId);
+
+                e.Property(v => v.Date)
+                .IsRequired()
+                .HasColumnType("DATETIME2")
+                .HasDefaultValueSql("GETDATE()");
+
+                e.Property(v => v.Comments)
+                .IsRequired(false)
+                .IsUnicode()
+                .HasMaxLength(250);
+
+                e.HasOne(v => v.Patient)
+                .WithMany(p => p.Visitations)
+                .HasForeignKey(v => v.PatientId)
+                .HasConstraintName("FK_Visitation_Patient");
+
+                e.HasOne(v => v.Doctor)
+                .WithMany(d => d.Visitations)
+                .HasForeignKey(v => v.DoctorId)
+                .HasConstraintName("FK_Visitation_Doctor");
+            });
+
+            modelBuilder.Entity<Diagnose>(e =>
+            {
+                e.HasKey(d => d.DiagnoseId);
+
+                e.Property(d => d.Name)
+                .IsRequired()
+                .IsUnicode()
+                .HasMaxLength(50);
+
+                e.Property(d => d.Comments)
+                .IsRequired(false)
+                .IsUnicode()
+                .HasMaxLength(250);
+
+                e.HasOne(d => d.Patient)
+                .WithMany(p => p.Diagnoses)
+                .HasForeignKey(d => d.PatientId)
+                .HasConstraintName("FK_Diagnose_Patient");
+            });
+
+            modelBuilder.Entity<Medicament>(e =>
+            {
+                e.HasKey(m => m.MedicamentId);
+
+                e.Property(m => m.Name)
+                .IsRequired()
+                .IsUnicode()
+                .HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<PatientMedicament>(e =>
+            {
+                e.HasKey(pm => new { pm.PatientId, pm.MedicamentId });
+
+                e.HasOne(pm => pm.Medicament)
+                .WithMany(m => m.Prescriptions)
+                .HasForeignKey(pm => pm.MedicamentId);
+
+                e.HasOne(pm => pm.Patient)
+                .WithMany(p => p.Prescriptions)
+                .HasForeignKey(pm => pm.PatientId);
+            });
+
+            modelBuilder.Entity<Doctor>(e =>
+            {
+                e.HasKey(d => d.DoctorId);
+
+                e.Property(d => d.Name)
+                .IsRequired()
+                .IsUnicode()
+                .HasMaxLength(100);
+
+                e.Property(d => d.Specialty)
+                .IsRequired()
+                .IsUnicode()
+                .HasMaxLength(100);
+            });
+        }
+    }
+}
---
+++ Databases Advanced Entity Framework/Code First/P03_SalesDatabase/Data/SalesContext .cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P03_SalesDatabase.Data.Models;
+using System.IO;
+
+namespace P03_SalesDatabase.Data
+{
+    public class SalesContext : DbContext
+    {
+        public SalesContext()
+        {
+
+        }
+
+        public SalesContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = File.ReadAllText("C:\\Program Files (x86)\\Notepad++\\Instructions.txt");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(e =>
+            {
+                e.HasKey(p => p.ProductId);
+
+                e.Property(p => p.Name).IsRequired().IsUnicode().HasMaxLength(50);
+
+                e.Property(p => p.Quantity).IsRequired();
+
+                e.Property(p => p.Price).IsRequired();
+
+                e.Property(p => p.Description).IsRequired().HasMaxLength(250).HasDefaultValue("No description");
+            });
+
+            modelBuilder.Entity<Customer>(e =>
+            {
+                e.HasKey(c => c.CustomerId);
+
+                e.Property(c => c.Name).IsRequired().IsUnicode().HasMaxLength(100);
+
+                e.Property(c => c.Email).IsUnicode(false).HasMaxLength(80);
+
+                e.Property(c => c.CreditCardNumber).IsRequired();
+            });
+
+            modelBuilder.Entity<Store>(e =>
+            {
+                e.HasKey(s => s.StoreId);
+
+                e.Property(s => s.Name).IsRequired().IsUnicode().HasMaxLength(80);
+            });
+
+            modelBuilder.Entity<Sale>(e =>
+            {
+                e.HasKey(s => s.SaleId);
+
+                e.Property(s => s.Date).IsRequired().HasColumnType("DATETIME2").HasDefaultValueSql("GETDATE()");
+
+                e.HasOne(s => s.Product).WithMany(p => p.Sales).HasForeignKey(s => s.ProductId);
+
+                e.HasOne(s => s.Customer).WithMany(c => c.Sales).HasForeignKey(s => s.CutomerId);
+
+                e.HasOne(s => s.Store).WithMany(s => s.Sales).HasForeignKey(s => s.StoreId);
+            });
+        }
+    }
+}
