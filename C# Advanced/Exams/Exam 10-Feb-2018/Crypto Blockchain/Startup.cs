7caa333f93ac35c138e1cab757d039962a49a5ae
BlueDress <stra6aro@abv.bg>
2018-03-17 18:43:54 +0200

    Update

---
+++ C# Advanced/Exams/Exam 10-Feb-2018/Crypto Blockchain/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CryptoBlockchain
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var numberOfLines = int.Parse(Console.ReadLine());
+
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                var line = Console.ReadLine();
+
+                sb.Append(line);
+            }
+
+            var blockChain = sb.ToString();
+            var pattern = @"[{\[].*?[}\]]";
+            var numberPattern = @"\d{3,}";
+
+            var matches = Regex.Matches(blockChain, pattern);
+
+            var cryptoBlocks = new List<string>();
+
+            foreach (Match match in matches)
+            {
+                var startSymbol = match.ToString()[0];
+                var lastSymbol = match.ToString()[match.Length - 1];
+
+                if (((startSymbol.Equals('[') && lastSymbol.Equals(']')) || ((startSymbol.Equals('{') && lastSymbol.Equals('}'))) && Regex.IsMatch(match.ToString(), numberPattern)))
+                {
+                    cryptoBlocks.Add(match.ToString());
+                }
+            }
+
+            sb.Clear();
+
+            foreach (var block in cryptoBlocks)
+            {
+                var validBlock = Regex.Match(block, numberPattern).ToString();
+
+                if (validBlock.Length % 3 == 0)
+                {
+                    for (int i = 0; i < validBlock.Length; i += 3)
+                    {
+                        var asciiNumber = int.Parse($"{validBlock[i]}{validBlock[i + 1]}{validBlock[i + 2]}") - block.Length;
+                        var symbol = Convert.ToChar(asciiNumber);
+
+                        sb.Append(symbol);
+                    }
+                }
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
---
+++ C# Advanced/Exams/Exam 10-Feb-2018/Hit List/Startup.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HitList
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var targetInfoIndex = int.Parse(Console.ReadLine());
+
+            var people = new Dictionary<string, Dictionary<string, string>>();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (input.Equals("end transmissions"))
+                {
+                    break;
+                }
+
+                var personInfo = input.Split(new[] { '=', ';' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                var personName = personInfo[0];
+
+                if (!people.ContainsKey(personName))
+                {
+                    people[personName] = new Dictionary<string, string>();
+                }
+
+                var personDetails = personInfo.Skip(1);
+
+                foreach (var personDetail in personDetails)
+                {
+                    var details = personDetail.Split(':');
+
+                    var info = details[0];
+                    var value = details[1];
+
+                    if (!people[personName].ContainsKey(info))
+                    {
+                        people[personName][info] = string.Empty;
+                    }
+
+                    people[personName][info] = value;
+                }
+            }
+
+            var killCommand = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var personToKill = killCommand[1];
+            var infoIndex = 0;
+
+            Console.WriteLine($"Info on {personToKill}:");
+
+            foreach (var info in people[personToKill].OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"---{info.Key}: {info.Value}");
+                infoIndex += info.Key.Length + info.Value.Length;
+            }
+
+            Console.WriteLine($"Info index: {infoIndex}");
+
+            if (infoIndex >= targetInfoIndex)
+            {
+                Console.WriteLine("Proceed");
+            }
+            else
+            {
+                Console.WriteLine($"Need {targetInfoIndex - infoIndex} more info.");
+            }
+        }
+    }
+}
