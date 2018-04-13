707af5491d7008e1d9d05a134621eaf5cf57d43b
BlueDress <stra6aro@abv.bg>
2018-04-13 15:42:10 +0300

    Update

--- Data Structures/Basic Tree Data Structures/ImplementTree/BinarySearchTree.cs
+++ Data Structures/Basic Tree Data Structures/ImplementTree/BinarySearchTree.cs
@@ -7,6 +7,8 @@ namespace ImplementTree
     {
         private Node root;
 
+        private int count;
+
         private class Node
         {
             public Node(T value)
@@ -43,9 +45,12 @@ namespace ImplementTree
 
         public void Insert(T value)
         {
+            this.count++;
+
             if (this.root.Value == null)
             {
                 this.root = new Node(value);
+                return;
             }
 
             Node parrent = null;
@@ -141,6 +146,8 @@ namespace ImplementTree
             {
                 parrent.LeftChild = currentNode.RightChild;
             }
+
+            this.count--;
         }
 
         public BinarySearchTree<T> Search(T value)
@@ -223,9 +230,113 @@ namespace ImplementTree
             this.EachInOrder(node.RightChild, action);
         }
 
-        public void Delete(T element)
+        public void Delete(T value)
         {
-            throw new NotImplementedException();
+            Node parent = null;
+            var currentNode = this.root;
+
+            while (true)
+            {
+                if (currentNode == null)
+                {
+                    break;
+                }
+
+                if (currentNode.Value.CompareTo(value) > 0)
+                {
+                    currentNode = currentNode.LeftChild;
+                }
+                else if (currentNode.Value.CompareTo(value) < 0)
+                {
+                    currentNode = currentNode.RightChild;
+                }
+                else
+                {
+                    break;
+                }
+
+                parent = currentNode;
+            }
+
+            var deleteRightChild = false;
+
+            if (parent.RightChild.Value.CompareTo(currentNode.Value) == 0)
+            {
+                deleteRightChild = true;
+            }
+
+            this.Delete(parent, currentNode, deleteRightChild);
+        }
+
+        private void Delete(Node parent, Node node, bool deleteRightChild)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            if (node.RightChild != null)
+            {
+                var currentNode = node.RightChild;
+
+                if (deleteRightChild)
+                {
+                    parent.RightChild = currentNode;
+                }
+                else
+                {
+                    parent.LeftChild = currentNode;
+                }
+
+                while (true)
+                {
+                    if (currentNode.LeftChild == null)
+                    {
+                        break;
+                    }
+
+                    currentNode = currentNode.LeftChild;
+                }
+
+                currentNode.LeftChild = node.LeftChild;
+
+                if (parent == null)
+                {
+                    this.root = currentNode;
+                }
+            }
+            else if (node.LeftChild != null)
+            {
+                if (deleteRightChild)
+                {
+                    parent.RightChild = node.LeftChild;
+                }
+                else
+                {
+                    parent.LeftChild = node.LeftChild;
+                }
+
+                if (parent == null)
+                {
+                    this.root = node.LeftChild;
+                }
+            }
+            else
+            {
+                if (deleteRightChild)
+                {
+                    parent.RightChild = null;
+                }
+                else
+                {
+                    parent.LeftChild = null;
+                }
+
+                if (parent == null)
+                {
+                    this.root = null;
+                }
+            }
         }
 
         public void DeleteMax()
@@ -257,31 +368,180 @@ namespace ImplementTree
             {
                 parrent.RightChild = currentNode.LeftChild;
             }
+
+            this.count--;
         }
 
         public int Count()
         {
-            throw new NotImplementedException();
+            return this.count;
         }
 
-        public int Rank(T element)
+        public int Rank(T value)
         {
-            throw new NotImplementedException();
+            var elements = new List<T>();
+
+            this.Rank(value, elements, this.root);
+
+            return elements.Count - 1;
+            //var currentNode = this.root;
+            //
+            //while (true)
+            //{
+            //    if (currentNode == null)
+            //    {
+            //        break;
+            //    }
+            //
+            //    if (currentNode.Value.CompareTo(value) > 0)
+            //    {
+            //        currentNode = currentNode.LeftChild;
+            //    }
+            //    else if (currentNode.Value.CompareTo(value) < 0)
+            //    {
+            //        currentNode = currentNode.RightChild;
+            //    }
+            //    else
+            //    {
+            //        break;
+            //    }
+            //}
+            //
+            //if (currentNode.LeftChild == null)
+            //{
+            //    return 0;
+            //}
+            //
+            //var count = 1;
+            //var queue = new Queue<Node>();
+            //queue.Enqueue(currentNode.LeftChild);
+            //
+            //while (true)
+            //{
+            //    if (queue.Count == 0)
+            //    {
+            //        break;
+            //    }
+            //
+            //    var current = queue.Dequeue();
+            //
+            //    if (current.LeftChild != null)
+            //    {
+            //        queue.Enqueue(current.LeftChild);
+            //        count++;
+            //    }
+            //
+            //    if (current.RightChild != null)
+            //    {
+            //        queue.Enqueue(current.RightChild);
+            //        count++;
+            //    }
+            //}
+            //
+            //return count;
+        }
+
+        private void Rank(T value, List<T> elements, Node node)
+        {
+            if (node.LeftChild != null)
+            {
+                this.Rank(value, elements, node.LeftChild);
+            }
+
+            elements.Add(node.Value);
+
+            if (node.Value.CompareTo(value) == 0)
+            {
+                return;
+            }
+
+            if (node.RightChild != null)
+            {
+                this.Rank(value, elements, node.RightChild);
+            }
         }
 
         public T Select(int rank)
         {
-            throw new NotImplementedException();
+            var elements = new List<T>();
+
+            this.Select(rank, elements, this.root);
+
+            return elements[rank];
         }
 
-        public T Ceiling(T element)
+        private void Select(int rank, List<T> elements, Node node)
         {
-            throw new NotImplementedException();
+            if (node.LeftChild != null)
+            {
+                this.Select(rank, elements, node.LeftChild);
+            }
+
+            elements.Add(node.Value);
+
+            if (elements.Count - 1 == rank)
+            {
+                return;
+            }
+
+            if (node.RightChild != null)
+            {
+                this.Select(rank, elements, node.RightChild);
+            }
         }
 
-        public T Floor(T element)
+        public T Ceiling(T value)
         {
-            throw new NotImplementedException();
+            return this.Ceiling(this.root, value);
+        }
+
+        private T Ceiling(Node node, T value)
+        {
+            if (node == null)
+            {
+                return node.Value;
+            }
+            
+            if (node.Value.CompareTo(value) <= 0)
+            {
+                return this.Ceiling(node.RightChild, value);
+            }
+
+            var ceiling = this.Ceiling(node.LeftChild, value);
+
+            if (ceiling == null)
+            {
+                return node.Value;
+            }
+
+            return ceiling;
+        }
+
+        public T Floor(T value)
+        {
+            return this.Floor(this.root, value);
+        }
+
+        private T Floor(Node node, T value)
+        {
+            if (node == null)
+            {
+                return node.Value;
+            }
+
+            if (node.Value.CompareTo(value) >= 0)
+            {
+                return this.Floor(node.LeftChild, value);
+            }
+
+            var floor = this.Floor(node.RightChild, value);
+
+            if (floor == null)
+            {
+                return node.Value;
+            }
+
+            return floor;
         }
     }
 }
